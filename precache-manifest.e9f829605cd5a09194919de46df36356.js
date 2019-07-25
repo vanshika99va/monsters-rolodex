35c685b075ee24cf3a6453b2f3c646c936f8efb5
vanshika <anandvanshika8@gmail.com>
2019-07-25 06:42:43 -0400

    Updates

---
+++ precache-manifest.e9f829605cd5a09194919de46df36356.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f54ddcf660f770ddd014c7ad0cca0d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "61d8cbb157487c293cd4",
+    "url": "/monsters-rolodex/static/css/main.179709a4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "61d8cbb157487c293cd4",
+    "url": "/monsters-rolodex/static/js/main.68286d31.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
